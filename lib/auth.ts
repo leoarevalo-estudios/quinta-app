e4ae425edbdecac4f0ef111b15086799b0e3a6cd
Leo Arevalo <leoarevalo.estudios@gmail.com>
2026-07-03 08:41:16 -0300

    Final-parcial

--- lib/auth.ts
+++ lib/auth.ts
@@ -4,6 +4,10 @@ import { prisma } from "./prisma";
 import bcrypt from "bcryptjs";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  session: {
+    strategy: "jwt",
+  },
+
   providers: [
     Credentials({
       name: "credentials",
@@ -11,6 +15,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         email: { label: "Email", type: "email" },
         password: { label: "Contraseña", type: "password" },
       },
+
       async authorize(credentials) {
         if (!credentials?.email || !credentials?.password) return null;
 
@@ -37,18 +42,23 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       },
     }),
   ],
+
   callbacks: {
     jwt({ token, user, trigger, session }) {
+      // login inicial
       if (user) {
         token.id = (user as any).id;
         token.role = (user as any).role;
         token.telefono = (user as any).telefono;
       }
 
-      if (trigger === "update") {
-        token.name = session.name;
-        token.email = session.email;
-        token.telefono = (session as any).telefono;
+      // cuando hacés await update()
+      if (trigger === "update" && session) {
+        if (session.name) token.name = session.name;
+        if (session.email) token.email = session.email;
+        if ((session as any).telefono !== undefined) {
+          token.telefono = (session as any).telefono;
+        }
       }
 
       return token;
@@ -66,6 +76,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       return session;
     },
   },
+
   pages: {
     signIn: "/login",
   },
