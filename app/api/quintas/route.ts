59bd14731852e2a17569ea8a004864df6af45372
Leo Arevalo <leoarevalo.estudios@gmail.com>
2026-07-03 08:32:23 -0300

    Final-parcial

--- app/api/quintas/route.ts
+++ app/api/quintas/route.ts
@@ -1,7 +1,8 @@
 import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 import { auth } from "@/lib/auth";
-
+export const runtime = "nodejs";
+export const dynamic = "force-dynamic";
 export async function GET(req: Request) {
   try {
     const { searchParams } = new URL(req.url);
